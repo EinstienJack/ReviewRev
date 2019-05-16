2c73aac286f6fc13a5fbde9fbc577b697593e93a
Einstien Jackson <Einstien Jackson@SuniJack>
2019-05-16 23:12:08 +0530

    Last update

--- april/src/test/java/com/reviewrev/april/createProjectTest.java
+++ april/src/test/java/com/reviewrev/april/createProjectTest.java
@@ -27,21 +27,28 @@ public class createProjectTest extends baseFile {
 	
 	@Test
 	public void creatProjectTest() {
-		String url = prop.getProperty("qaURL");	
-		driver.get(url);
-		sign = log.navigateCreateAccount();			
+		String url = prop.getProperty("qaURL");
 		excelUtility.ShName = "CreateProject";		
 		Sheet sh = excelUtility.readExcel();
-		int lastRow = sh.getLastRowNum();			
-		for (int i=1; i<=3; i++) {			
+		int lastRow = sh.getLastRowNum();				
+				
+		for (int i=1; i<=lastRow; i++) {		
+			driver.get(url);
+			sign = log.navigateCreateAccount();	
+			// if status was null execute this 
+			// Handle invalid email and existing mail 
+			
 			String a = excelUtility.getvalue(i, 0); 
 			String b = excelUtility.getvalue(i, 1);
 			String c = excelUtility.getvalue(i, 2);
 			String d = excelUtility.getvalue(i, 3);
 			String e = excelUtility.getvalue(i, 4);			
 			sign.creatProject(a,b,c,d,e);
-			
-			
+			String Url = driver.getCurrentUrl();
+			String status = "Pass";			
+			excelUtility.setvalue(status, Url, i);
+			driver.manage().deleteAllCookies();
+			driver.close();
 		}	
 	}
 	
